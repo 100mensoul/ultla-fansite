b84f5e70587b2d9a7249f1ff72756ec1a34ecb57
100mensoul <momo2ro1260@gmail.com>
2025-05-12 15:51:49 +0900

    Create sw.js

---
+++ airtree/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'fieldnote-cache-v1';
+const urlsToCache = [
+  'index.html',
+  'main.js',
+  'db.js',
+  'manifest.json',
+  'style.css',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
