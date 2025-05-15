6234706e12a7e812cb70a4bbb88ea3fd44326664
100mensoul <momo2ro1260@gmail.com>
2025-05-15 17:20:43 +0900

    sw.js を更新

--- airtree/sw.js
+++ airtree/sw.js
@@ -1,39 +1,26 @@
-const CACHE_NAME = 'fieldnote-cache-v1';
-const urlsToCache = [
-  'index.html',
-  'main.js',
-  'db.js',
-  'manifest.json',
-  'style.css',
-  'icon-192.png',
-  'icon-512.png'
-];
+// このService Workerは登録直後に自らアンレジスターされ、キャッシュを削除します。
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', function(event) {
+  console.log('[sw.js] install - 即時アクティベート');
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', function(event) {
+  console.log('[sw.js] activate - キャッシュとSW登録を削除します');
+
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
+        cacheNames.map(cache => caches.delete(cache))
       );
-    })
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    }).then(() => {
+      return self.registration.unregister();
+    }).then(() => {
+      return self.clients.matchAll();
+    }).then(clients => {
+      clients.forEach(client => {
+        client.navigate(client.url); // 強制リロード
+      });
     })
   );
 });
