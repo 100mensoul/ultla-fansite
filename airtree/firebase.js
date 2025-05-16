81c3efba3a1c8e334827ccebbe57ad75098dd03f
100mensoul <momo2ro1260@gmail.com>
2025-05-16 18:56:20 +0900

    firebase.js を更新

--- airtree/firebase.js
+++ airtree/firebase.js
@@ -4,14 +4,15 @@ import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gsta
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-storage.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCtDPnYex-KL2hbHAQe5fYSPv9rz9xTa9A",
-  authDomain: "u2memo-36f61.firebaseapp.com",
-  projectId: "u2memo-36f61",
-  storageBucket: "u2memo-36f61.appspot.com",
-  messagingSenderId: "14274931072",
-  appId: "1:14274931072:web:5d9c9026905fdc0b383965"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCtDPnYex-KL2hbHAQe5fYSPv9rz9xTa9A",
+    authDomain: "u2memo-36f61.firebaseapp.com",
+    databaseURL: "https://u2memo-36f61-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "u2memo-36f61",
+    storageBucket: "u2memo-36f61.firebasestorage.app",
+    messagingSenderId: "14274931072",
+    appId: "1:14274931072:web:5d9c9026905fdc0b383965"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
