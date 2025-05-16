a83871029cee9417a152dd7d1045d51b2831a644
100mensoul <momo2ro1260@gmail.com>
2025-05-16 23:40:57 +0900

    firebase.js を更新

--- airtree/firebase.js
+++ airtree/firebase.js
@@ -4,28 +4,35 @@ import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gsta
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCtDPnYex-KL2hbHAQe5fYSPv9rz9xTa9A",
-    authDomain: "u2memo-36f61.firebaseapp.com",
-    databaseURL: "https://u2memo-36f61-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "u2memo-36f61",
-    storageBucket: "u2memo-36f61.firebasestorage.app",
-    messagingSenderId: "14274931072",
-    appId: "1:14274931072:web:5d9c9026905fdc0b383965"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCtDPnYex-KL2hbHAQe5fYSPv9rz9xTa9A",
+  authDomain: "u2memo-36f61.firebaseapp.com",
+  databaseURL: "https://u2memo-36f61-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "u2memo-36f61",
+  storageBucket: "u2memo-36f61.appspot.com",  // 修正: .app → .appspot.com
+  messagingSenderId: "14274931072",
+  appId: "1:14274931072:web:5d9c9026905fdc0b383965"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-// ログイン完了を待つ Promise を提供
+// 匿名ログイン処理（authReady）
 const authReady = new Promise((resolve, reject) => {
   onAuthStateChanged(auth, user => {
     if (user) {
       resolve(user);
     } else {
-      signInAnonymously(auth).catch(reject);
+      signInAnonymously(auth)
+        .then(() => {
+          onAuthStateChanged(auth, user => {
+            if (user) resolve(user);
+            else reject(new Error("認証に失敗しました"));
+          });
+        })
+        .catch(reject);
     }
   });
 });
